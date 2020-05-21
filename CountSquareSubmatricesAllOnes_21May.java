fb535ab713e40a89ce011a1e63eb46e8e02fe73d
Ankit Ratnawat <ratnawat.ankit1@gmail.com>
2020-05-21 23:51:56 +0530

    replaced with space Optimized solution - 21 May

--- CountSquareSubmatricesAllOnes_21May.java
+++ CountSquareSubmatricesAllOnes_21May.java
@@ -1,27 +1,25 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-        int[][] dp = new int[matrix.length][matrix[0].length];
-        
         int count = 0;
         for(int i=0;i<matrix.length;i++) {
-            dp[i][0] = matrix[i][0];
-            count = count+dp[i][0];
+            if(matrix[i][0] == 1) {
+                count = count+matrix[i][0];
+            }
         }
         
         for(int i=1;i<matrix[0].length;i++) {
-            dp[0][i] = matrix[0][i];
-            count = count+dp[0][i];
+            if(matrix[0][i] == 1) {
+                 count = count+matrix[0][i];
+            }
         }
         
-        
+    
         for(int i=1;i<matrix.length;i++) {
             for(int j=1;j<matrix[0].length;j++) {
-                if(matrix[i][j] ==0) {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])+1;
-                    count = count + dp[i][j];
-                }
+                if(matrix[i][j] ==1) {
+                    matrix[i][j] = min(matrix[i-1][j-1],matrix[i-1][j],matrix[i][j-1])+1;
+                    count = count + matrix[i][j]; 
+                } 
             }
         }
         return count;
